778556f2627dd217c3d7265d78ee75074a33d98e
ayush-raj8 <ayushraj763@gmail.com>
2024-09-28 14:58:01 +0530

    build removed from .gitignore of build-branch'

--- server.js
+++ server.js
@@ -3,25 +3,12 @@ const path = require('path');
 
 const app = express();
 
-
-process.env.NODE_ENV = "PRODUCTION1"; 
-
-if (process.env.NODE_ENV === "PRODUCTION") {
-  app.use((req, res, next) => {
-    if (req.header('x-forwarded-proto') !== 'https') {
-      res.redirect(`https://${req.header('host')}${req.url}`);
-    } else {
-      next();
-    }
-  });
-}
-
 // Serve the static files from the React app
-app.use(express.static(path.join(__dirname, './build')));
+app.use(express.static(path.join(__dirname, 'build')));
 
 // Handles any requests that don't match the ones above
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 // Start the server
