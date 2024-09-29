4004da86bfb2832ed205bcec86db353c299daf98
ayush-raj8 <ayushraj763@gmail.com>
2024-09-29 17:59:33 +0530

    29 Th Sept 6PM build

---
+++ build/static/js/1701.4b993ade.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunknritya_webApp=self.webpackChunknritya_webApp||[]).push([[1701],{51701:(e,n,t)=>{t.r(n),t.d(n,{default:()=>r});t(72791);var p=t(80184);const r=function(){return(0,p.jsx)("div",{})}}}]);
+//# sourceMappingURL=1701.4b993ade.chunk.js.map
