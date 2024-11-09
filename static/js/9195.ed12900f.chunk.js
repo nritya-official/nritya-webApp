1d8105110d0b6bb4f6d9890eeb90edbfd047d3f9
ayush-raj8 <ayushraj763@gmail.com>
2024-11-09 12:11:06 +0530

    9/11/24 Build

---
+++ static/js/9195.ed12900f.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunknritya_webApp=self.webpackChunknritya_webApp||[]).push([[9195],{29195:(e,t,a)=>{a.r(t),a.d(t,{default:()=>o});a(72791);var n=a(5180),c=a(83708),l=a(39441),p=a(80184);const o=function(){return(0,p.jsx)(n.u,{children:(0,p.jsx)(l.Z,{entityCollectionName:c.Ul.WORKSHOPS,storageCollectionName:c.Rb.WORKSHOPICON,defaultImageUrl:"https://cdn.pixabay.com/photo/2016/12/30/10/03/dance-1940245_960_720.jpg"})})}}}]);
+//# sourceMappingURL=9195.ed12900f.chunk.js.map
