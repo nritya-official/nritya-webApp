df1c88fce44582dc06b35ff94ae077db837a9a70
ayush-raj8 <ayushraj763@gmail.com>
2024-11-09 12:14:21 +0530

    9/11/24

---
+++ build/build/static/js/9995.fc1cd985.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunknritya_webApp=self.webpackChunknritya_webApp||[]).push([[9995],{51701:(e,n,t)=>{t.r(n),t.d(n,{default:()=>r});t(72791);var p=t(80184);const r=function(){return(0,p.jsx)("div",{})}}}]);
+//# sourceMappingURL=9995.fc1cd985.chunk.js.map
