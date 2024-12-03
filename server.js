157af736d73c2c42594ab7f4e699b70eef32e33e
ayush-raj8 <ayushraj763@gmail.com>
2024-12-03 18:00:33 +0530

    Pre-render setup-11

--- server.js
+++ server.js
@@ -4,18 +4,18 @@ const prerenderMiddleware = require('prerender-node');
 const prerender = require('prerender');
 
 const app = express();
-/*
+
 // Start Prerender server in the same process
 const prerenderServer = prerender({
   chromeLocation: '/app/.chrome-for-testing/chrome-linux64/chrome', // Heroku environment variable
   logRequests: true, // Optional Logs requests
+  port: 5001,
 });
 
 // Set up Prerender middleware to use the internal server
 app.use(
   prerenderMiddleware.set('prerenderServiceUrl', 'http://localhost:5001/') // Use local Prerender server
 );
-*/
 
 // Serve the static files from the React app
 app.use(express.static(path.join(__dirname, 'build')));
