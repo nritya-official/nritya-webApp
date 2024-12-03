b8b0a267785eb7678e3747fbd22bd3e4b421990c
ayush-raj8 <ayushraj763@gmail.com>
2024-12-03 17:22:00 +0530

    Pre-render setup-4

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 
 // Start Prerender server in the same process
 const prerenderServer = prerender({
-  chromeLocation: '/usr/bin/google-chrome-stable', // Heroku environment variable
+  chromeLocation: '/app/.chrome-for-testing/chrome-linux64/chrome', // Heroku environment variable
   logRequests: true, // Optional Logs requests
 });
 prerenderServer.start();
